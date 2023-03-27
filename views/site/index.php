96a1048eddc8730bd058b643875dd5b1bc65aefa
GuilhermeHMFroes <guil.henrique.fr@gmail.com>
2023-03-27 15:55:24 -0300

    Configurando o layout principal

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,23 @@
 <?php
 
-/** @var yii\web\View $this */
+///** @var yii\web\View $this */
 
-$this->title = 'GVI 3D';
+//$this->title = 'GVI 3D';
+
+use yii\helpers\Html;
+
+$this->title = 'GVI-3D';
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
-<div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+<?php foreach ($produtos as $produto) : ?>
+    <div class="produto">
+        <h2  style="text-align: center;"><?= $produto->nome ?></h2>
+        <img src="<?= $produto->imagem ?>" alt="<?= $produto->nome ?>" style="display: block; margin: 0 auto;">
+        <br>
+        <p style="text-align: justify;"><?= $produto->descricao ?></p>
+        <p>Valor: R$ <?= number_format($produto->valor, 2, ',', '.') ?></p>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-</div>
+<?php endforeach; ?>
